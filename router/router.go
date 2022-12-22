0a8862a295f1a390ef80e726542c6352f3343873
FDongFDong <abnormal5626@naver.com>
2022-12-22 10:19:52 +0900

    Add CORS 적용

--- router/router.go
+++ router/router.go
@@ -14,9 +14,24 @@ func NewRouter(ct *controller.Controller) (*Router, error) {
 	r := &Router{ct: ct}
 	return r, nil
 }
+func CORS() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, X-Forwarded-For, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	}
+}
+
 func (r *Router) Index() *gin.Engine {
 	gin.SetMode(gin.DebugMode)
 	e := gin.Default()
+	e.Use(CORS())
 	e.GET("/")
 	return e
 }
