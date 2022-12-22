c0fbdf7bfc975691cc4033a16c03e6a5f301555c
FDongFDong <abnormal5626@naver.com>
2022-12-22 10:04:23 +0900

    Add config 세팅

--- conf/config.go
+++ conf/config.go
@@ -1,1 +1,42 @@
 package conf
+
+import (
+	"os"
+
+	"github.com/naoina/toml"
+)
+
+type Config struct {
+	Server struct {
+		Mode string
+		Port string
+	}
+	Log struct {
+		Level   string
+		Fpath   string
+		Msize   int
+		Mage    int
+		Mbackup int
+	}
+}
+
+// level = "debug" # debug or info
+// fpath = "./logs/go-loger" # 로그가 생성될 경로 : ./logs, 로그파일명 go-loger_xxx.log
+// msize = 2000    # 2g : megabytes
+// mage = 7        # 7days
+// mbackup = 5    # number of log files
+func NewConfig(fpath string) *Config {
+	c := new(Config)
+
+	if file, err := os.Open(fpath); err != nil {
+		panic(err)
+	} else {
+		defer file.Close()
+		//toml 파일 디코딩
+		if err := toml.NewDecoder(file).Decode(c); err != nil {
+			panic(err)
+		} else {
+			return c
+		}
+	}
+}
